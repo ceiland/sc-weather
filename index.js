6caac9339908f50f34d7a735bf803a8dec404f15
unknown <ceiland@gmail.com>
2015-07-27 11:16:52 -0700

    'draft-04'

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var regex = require('regex');
 
 var app = express();
 
+var logFile = 'log.txt'
+var zipCode = new regex(/./);
+
 app.set('port', (process.env.PORT || 9001));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.listen(app.get('port'), function () {
-    console.log('Now listening on port ', app.get('port'));
+    console.log('Now listening on port ', app.get('port'))
 });
 
 app.post('/', function (req, res) {
-    var zip = req.body
-    res.send('You requested weather for zip: ' + zip)
+    console.log('Receiving a message...')
+    console.dir(req.body)
+    var zip = req.body.text
+    console.log('Request text = ' + zip)
+    console.log('Request regex vs \d = ' + zipCode.test(zip))
+
+});
+
+app.get('/', function (req, res) {
+    res.send('Get out')
 });
