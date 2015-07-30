52fe46247d9bffa41f9465c76bafa2b17da5e095
unknown <ceiland@gmail.com>
2015-07-29 17:14:44 -0700

    draft-06

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var fs = require('fs');
 var http = require('http');
 
 var app = express();
 
-var logFile = 'log.txt'
 var zipCheck = new RegExp(/^\d{5}$/);
 var tokenCheck = new RegExp('' + process.env.TOKEN);
 
@@ -17,58 +15,84 @@ app.listen(app.get('port'), function () {
 });
 
 app.post('/', function (req, res) {
-    var zip = req.body.text
-    var token = req.body.token
-    var fullUserName = req.body.user_name + '@' + req.body.team_domain
-    var weatherData = ''
+    var weatherPostData = ''
 
-    var weather = {
+    var slackData = {
+        token: req.body.token,
+        team_id: req.body.team_id,
+        team_domain: req.body.team_domain,
+        channel_id: req.body.channel_id,
+        channel_name: req.body.channel_name,
+        user_id: req.body.user_id,
+        user_name: req.body.user_name,
+        command: req.body.command,
+        zip: req.body.text,
+        fullUserName: req.body.user_name + '@' + req.body.team_domain + ' on #' + req.body.channel_name
+    }
+
+    var weatherData = {
+        cityLat: '',
+        cityLon: '',
+        weatherConditions: '',
+        weatherDescription: '',
         currentTemp: '',
         lowTemp: '',
         highTemp: '',
-        weatherConditions: '',
         humidityPercentage: '',
+        pressure: '',
         windSpeed: '',
+        windDirection: '',
         cloudPercentage: '',
+        rainfall: '',
+        snowfall: '',
+        timestamp: '',
+        sunrise: '',
+        sunset: '',
         cityName: '',
-        cityLat: '',
-        cityLon: '',
-        cityCountry: ''
+        cityCountry: '',
+        responseCode: ''
     }
 
     // Filter requests with bad tokens
-    if (!tokenCheck.test(token)) {
+    if (!tokenCheck.test(slackData.token)) {
         console.warn('WARNING: Bad POST body (' + req.ip + ')')
-        console.warn('\t\tIncorrect Slack API token: ' + token)
-        console.warn('\t\tCommand Payload: ' + req.body)
+        console.warn('\t\tIncorrect Slack API token: ' + slackData.token)
+        console.warn('\t\tCommand Payload: ')
+        console.dir(req.body)
         return 0
     }
 
     /* Debug
     console.log('Receiving a message...')
     console.dir(req.body)
-    console.log('Request text = \"' + zip + '\"')
-    console.log('Checking... ' + zipCheck.test(zip))
+    console.log('Request text = \"' + slackData.zip + '\"')
+    console.log('Checking... ' + zipCheck.test(slackData.zip))
     */
 
-    if (!zipCheck.test(zip)) {
-        res.send('Hi! I am currently hacking you. \nThe value you just sent me is NOT A ZIP CODE.')
+    if (!zipCheck.test(slackData.zip)) {
+        console.log('(' + slackData.fullUserName + ') just asked for weather info for INVALID zip code: ' + slackData.zip)
+        console.dir(res.body)
+        res.send('Hi! I am currently hacking you. \nThe value you just sent me is NOT A ZIP CODE: ' + slackData.zip)
     }
     else {
         /* Debug
-        res.send('Hi! You (' + fullUserName + ') just asked for weather info in zip code: ' + zip)
+        res.send('Hi! You (' + slackData.fullUserName + ') just asked for weather info in zip code: ' + slackData.zip)
         */
 
-        console.log('(' + fullUserName + ') just asked for weather info in zip code: ' + zip)
+        console.log('(' + slackData.fullUserName + ') just asked for weather info for zip code: ' + slackData.zip)
 
-        var postOptions = {
+        var weatherPostOptions = {
             host: 'api.openweathermap.org',
             port: 80,
-            path: '/data/2.5/weather?zip=' + zip + ',us&units=imperial&APPID=' + process.env.APPID,
+            path: '/data/2.5/weather?zip=' + slackData.zip + ',us&units=imperial&APPID=' + process.env.APPID,
             method: 'POST'
         }
 
-        var request = http.request(postOptions, function (response) {
+        var slackPostOptions = {
+
+        }
+
+        var request = http.request(weatherPostOptions, function (response) {
             /* Debug
             console.log('\nBEGIN HTTP POST\n----------------------');
             console.log('STATUS:\t' + response.statusCode);
@@ -78,7 +102,7 @@ app.post('/', function (req, res) {
             response.setEncoding('utf8');
 
             response.on('data', function (chunk) {
-                weatherData += chunk
+                weatherPostData += chunk
             });
 
             response.on('end', function () {
@@ -87,7 +111,7 @@ app.post('/', function (req, res) {
                 */
 
                 // Iterator method
-                /*JSON.parse(weatherData, function (key, val) {
+                /*JSON.parse(weatherPostData, function (key, val) {
                     switch (key) {
                         case 'temp':
                             console.log('Current Temp: ' + val);
@@ -103,30 +127,30 @@ app.post('/', function (req, res) {
                 })*/
 
                 // Array Method
-                weatherData = JSON.parse(weatherData)
+                weatherPostData = JSON.parse(weatherPostData)
 
-                switch (weatherData['cod'].toString()) {
+                switch (weatherPostData['cod'].toString()) {
                     case '404':
-                        res.send(weatherData['message']);
+                        res.send(weatherPostData['message']);
                         break;
                     case '200':
-                        weather.cityName = weatherData['name'];
-                        weather.cityLat = weatherData['coord']['lat'];
-                        weather.cityLon = weatherData['coord']['lon'];
-                        weather.cityCountry = weatherData['sys']['country']
+                        weatherData.cityName = weatherPostData['name'];
+                        weatherData.cityLat = weatherPostData['coord']['lat'];
+                        weatherData.cityLon = weatherPostData['coord']['lon'];
+                        weatherData.cityCountry = weatherPostData['sys']['country']
 
-                        weather.weatherConditions = weatherData['weather'][0]['main'];
-                        weather.currentTemp = Math.round(weatherData['main']['temp']);
-                        weather.lowTemp = Math.round(weatherData['main']['temp_min']);
-                        weather.highTemp = Math.round(weatherData['main']['temp_max']);
-                        weather.humidityPercentage = weatherData['main']['humidity'];
+                        weatherData.weatherConditions = weatherPostData['weather'][0]['main'];
+                        weatherData.currentTemp = Math.round(weatherPostData['main']['temp']);
+                        weatherData.lowTemp = Math.round(weatherPostData['main']['temp_min']);
+                        weatherData.highTemp = Math.round(weatherPostData['main']['temp_max']);
+                        weatherData.humidityPercentage = weatherPostData['main']['humidity'];
 
-                        weather.windSpeed = weatherData['wind']['speed'];
-                        weather.cloudPercentage = weatherData['clouds']['all'];
+                        weatherData.windSpeed = weatherPostData['wind']['speed'];
+                        weatherData.cloudPercentage = weatherPostData['clouds']['all'];
 
-                        var outputMain = 'Weather for ' + weather.cityName + ', ' + weather.cityCountry + ' (' + weather.cityLat + ', ' + weather.cityLon + ')\n'
-                        var outputTemperature = 'Temperature: ' + weather.currentTemp + ' F (' + weather.lowTemp + ' / ' + weather.highTemp + ')\n'
-                        var outputConditions = 'Conditions: ' + weather.weatherConditions;
+                        var outputMain = 'Weather for ' + weatherData.cityName + ', ' + weatherData.cityCountry + ' (' + weatherData.cityLat + ', ' + weatherData.cityLon + ')\n'
+                        var outputTemperature = 'Temperature: ' + weatherData.currentTemp + ' F (' + weatherData.lowTemp + ' / ' + weatherData.highTemp + ')\n'
+                        var outputConditions = 'Conditions: ' + weatherData.weatherConditions;
 
                         res.send(outputMain + outputTemperature + outputConditions);
                         break;
